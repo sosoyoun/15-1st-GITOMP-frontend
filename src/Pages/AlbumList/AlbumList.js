c1784fc0f4c66581fdf872d01d4128dfef9d1021
9bin08 <9bin08@naver.com>
2020-12-18 20:20:23 +0900

    Add: 폼구현중

--- src/Pages/AlbumList/AlbumList.js
+++ src/Pages/AlbumList/AlbumList.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import "./AlbumList.scss";
 
 class AlbumList extends Component {
+  handleInputChange = (e) => {
+    this.setState({ searchInput: e.target.value });
+  };
   render() {
-    return <div>gh2sdfsdfsdf</div>;
+    return (
+      <div className="AlbumList">
+        <div className="container">
+          <div className="headtitle">
+            <h2>AlbumList</h2>
+            <span>깃톰프뮤직의 음악에 매료되어보세요.</span>
+          </div>
+          <section className="notice-info">
+            <form>
+              <select name="findType" tabIndex="1">
+                <option value="all">전체</option>
+                <option value="name">제목</option>
+                <option value="content">내용</option>
+              </select>
+              <input
+                type="text"
+                placeholder="검색어를 입력해 주세요"
+                onChange={this.handleInputChange}
+              />
+              <button>
+                <FontAwesomeIcon className="fa-search" icon={faSearch} />
+              </button>
+            </form>
+          </section>
+        </div>
+      </div>
+    );
   }
 }
 
