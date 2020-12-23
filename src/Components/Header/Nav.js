8ddbaf3bed4ccd367b8df9cc28c9f5b337464201
sosoyoun <ss3152psy@gmail.com>
2020-12-24 05:19:33 +0900

    Add : 유저 로그인 폼

--- src/Components/Header/Nav.js
+++ src/Components/Header/Nav.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Category from "./components/Category";
 import SearchBar from "./SearchBox/SearchBar";
 import "./Nav.scss";
 
 class Nav extends Component {
   render() {
+    const { handleSearchBox } = this;
     return (
       <div className="Nav bottom">
         <div className="Nav-top">
@@ -14,6 +18,16 @@ class Nav extends Component {
             <img src="/images/logo.gif" alt="GITOMP" />
           </Link>
           <div className="right-icon">
+            <div className="login_user-nickname">
+              <FontAwesomeIcon
+                icon={faUser}
+                size="1x"
+                className="user-nickname-icon"
+              />
+              <p>
+                <span>{/* {user_nickname} */}기톰프 님</span>
+              </p>
+            </div>
             <SearchBar />
             <div
               className="toggle-menu"
@@ -31,7 +45,7 @@ class Nav extends Component {
             </div>
           </div>
         </div>
-        <Category onSearchBox={this.handleSearchBox} />
+        <Category onSearchBox={handleSearchBox} />
       </div>
     );
   }
